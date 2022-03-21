35e7bb294cbda36450a09e1d5e8d85c78b19b177
Roland Leo Del Rosario <rleoadr@gmail.com>
2022-03-21 18:49:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let greetings = [];
+    for(let i=.0; i < names.length; i++) {
+        greetings.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+        return greetings;
+}
+function countDown() {
+    let i = 10;
+    while (i >= 0)
+    {
+        console.log(i--);
+    }
+}
